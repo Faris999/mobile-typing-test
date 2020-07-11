5d72077533a32fdc8da6a3af548d574b8512093f
Faris Hafizhan Hakim <farishafiz999@gmail.com>
2020-07-11 10:46:39 +0700

    Updates

---
+++ precache-manifest.9ce9e12f4aaefc6ceacd15fd93daa437.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b95f90398e71833d876256ba9364897",
+    "url": "/mobile-typing-test/index.html"
+  },
+  {
+    "revision": "fae098040a48428a643d",
+    "url": "/mobile-typing-test/static/css/main.2e8c32c3.chunk.css"
+  },
+  {
+    "revision": "4b169750707859f6c696",
+    "url": "/mobile-typing-test/static/js/2.eb990e5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mobile-typing-test/static/js/2.eb990e5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fae098040a48428a643d",
+    "url": "/mobile-typing-test/static/js/main.ea374d5f.chunk.js"
+  },
+  {
+    "revision": "1ab4bcb8520c8c0d5ecb",
+    "url": "/mobile-typing-test/static/js/runtime-main.2b1f7619.js"
+  }
+]);
